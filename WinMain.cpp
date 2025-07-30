fc313e72e4e6fa72a4ec3875cfa34d476946d20c
RadAd <adamgates84+github@gmail.com>
2025-07-30 15:15:51 +1000

    Sync

--- WinMain.cpp
+++ WinMain.cpp
@@ -4,78 +4,80 @@
 #include <tchar.h>
 #include <exception>
 #include <system_error>
-//#include <objbase.h>
-//#include <commctrl.h>
-
-//#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#include "WinMainSetup.h"
 
 HINSTANCE g_hInstance = NULL;
 HACCEL g_hAccelTable = NULL;
 HWND g_hWndAccel = NULL;
 HWND g_hWndDlg = NULL;
+HWND g_hWndMDIClient = NULL;
 
 bool Run(_In_ const LPCTSTR lpCmdLine, _In_ const int nShowCmd);
 
-int DoMessageLoop()
+namespace
 {
-    MSG msg;
-    while (GetMessage(&msg, nullptr, 0, 0))
+    int DoMessageLoop()
     {
-        if ((g_hWndDlg == NULL || !IsDialogMessage(g_hWndDlg, &msg))
-            && (g_hAccelTable == NULL || !TranslateAccelerator(g_hWndAccel, g_hAccelTable, &msg)))
+        MSG msg;
+        while (GetMessage(&msg, nullptr, 0, 0))
         {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+            if ((g_hWndDlg == NULL || !IsDialogMessage(g_hWndDlg, &msg))
+                && (g_hWndMDIClient == NULL || !TranslateMDISysAccel(g_hWndMDIClient, &msg))
+                && (g_hAccelTable == NULL || !TranslateAccelerator(g_hWndAccel, g_hAccelTable, &msg)))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
         }
+        return int(msg.wParam);
     }
-    return int(msg.wParam);
-}
 
-void DisplayError(const std::exception& e, const char* title)
-{
-    MessageBoxA(NULL, e.what(), title, MB_ICONERROR | MB_OK);
-}
+    void DisplayError(const std::exception& e, const char* title)
+    {
+        MessageBoxA(NULL, e.what(), title, MB_ICONERROR | MB_OK);
+    }
 
 #if _DEBUG
-void __cdecl CrtDebugEnd()
-{
-    _CrtMemState state;
-    _CrtMemCheckpoint(&state);
-
-    if (state.lCounts[_CLIENT_BLOCK] != 0 ||
-        state.lCounts[_NORMAL_BLOCK] != 0 ||
-        (_crtDbgFlag & _CRTDBG_CHECK_CRT_DF && state.lCounts[_CRT_BLOCK] != 0))
+    void __cdecl CrtDebugEnd()
     {
-        _RPT0(_CRT_ERROR, "Detected memory leaks!\n");
-    }
-}
+        _CrtMemState state;
+        _CrtMemCheckpoint(&state);
 
-void CrtDebugInitFile(LPCTSTR lpDebugFileName)
-{
-    const HANDLE hLogFile = CreateFile(lpDebugFileName, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    _ASSERTE(hLogFile);
-    if (hLogFile && hLogFile != INVALID_HANDLE_VALUE)
-    {
-        for (int e = _CRT_WARN; e < _CRT_ERRCNT; ++e)
+        if (state.lCounts[_CLIENT_BLOCK] != 0 ||
+            state.lCounts[_NORMAL_BLOCK] != 0 ||
+            (_crtDbgFlag & _CRTDBG_CHECK_CRT_DF && state.lCounts[_CRT_BLOCK] != 0))
         {
-            _CrtSetReportMode(e, _CrtSetReportMode(e, _CRTDBG_REPORT_MODE) | _CRTDBG_MODE_FILE);
-            _CrtSetReportFile(e, hLogFile);
+            _RPT0(_CRT_ERROR, "Detected memory leaks!\n");
         }
     }
-}
 
-void CrtDebugInit()
-{
-    TCHAR Filename[MAX_PATH];
-    const DWORD len = GetModuleFileName(NULL, Filename, ARRAYSIZE(Filename));
-    _ASSERTE(_tcscmp(Filename + len - 4, TEXT(".exe")) == 0);
-    _tcscpy_s(Filename + len - 4, ARRAYSIZE(Filename) - len + 4, TEXT("DbgLog.txt"));
-    CrtDebugInitFile(Filename);
-    atexit(CrtDebugEnd);
-}
+    void CrtDebugInitFile(LPCTSTR lpDebugFileName)
+    {
+        const HANDLE hLogFile = CreateFile(lpDebugFileName, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+        _ASSERTE(hLogFile);
+        if (hLogFile && hLogFile != INVALID_HANDLE_VALUE)
+        {
+            for (int e = _CRT_WARN; e < _CRT_ERRCNT; ++e)
+            {
+                _CrtSetReportMode(e, _CrtSetReportMode(e, _CRTDBG_REPORT_MODE) | _CRTDBG_MODE_FILE);
+                _CrtSetReportFile(e, hLogFile);
+            }
+        }
+    }
+
+    void CrtDebugInit()
+    {
+        TCHAR Filename[MAX_PATH];
+        const DWORD len = GetModuleFileName(NULL, Filename, ARRAYSIZE(Filename));
+        _ASSERTE(_tcscmp(Filename + len - 4, TEXT(".exe")) == 0);
+        _tcscpy_s(Filename + len - 4, ARRAYSIZE(Filename) - len + 4, TEXT("DbgLog.txt"));
+        CrtDebugInitFile(Filename);
+        atexit(CrtDebugEnd);
+    }
 #else
 #define CrtDebugInit()                   ((int)0)
 #endif
+}
 
 int WINAPI _tWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nShowCmd)
 try
---
+++ WinMainSetup.h
@@ -0,0 +1,6 @@
+// Uncomment lines to setup in WinMain
+
+//#include <objbase.h>
+//#include <commctrl.h>
+
+//#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
